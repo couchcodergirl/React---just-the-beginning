ba92e32c052ff31801b4d7705e44b90c73eaaa28
Emilia Piechocka <couchcodergirl@gmail.com>
2020-07-08 13:27:47 +0200

    Add List in App

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,10 +4,9 @@ import styles from './List.scss';
 class List extends React.Component {
   render() {
     return (
-      <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>Hello world!</h2>
-      </main>
+      <section className={styles.component}>
+        <h2 className={styles.subtitle}>Things to do</h2>
+      </section>
     )
   }
 }
