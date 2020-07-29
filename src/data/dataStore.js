96dea9a0170fcf29345cd71b2af49d341d753e17
Emilia Piechocka <couchcodergirl@gmail.com>
2020-07-29 15:44:39 +0200

    Install react-html-parser

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -12,7 +12,7 @@ export const settings = {
 
 export const pageContents = {
   title: 'My first React app',
-  subtitle: 'A simple to-do app, with lists, columns and cards',
+  subtitle: 'A simple to-do app, with lists, columns and cards and even more',
 };
 
 export const listData = {
