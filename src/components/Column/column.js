66ca99e0ea676c015f3311d540e7e2ec46101d7f
Emilia Piechocka <couchcodergirl@gmail.com>
2020-08-13 11:43:20 +0200

    Refactor lists

--- src/components/Column/column.js
+++ src/components/Column/column.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-import Card from '../Card/Card.js';
-import Creator from '../Creator/Creator.js';
+//import Card from '../Card/Card.js';
+//import Creator from '../Creator/Creator.js';
 import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
 
@@ -21,26 +21,8 @@ class Column extends React.Component {
     description: settings.defaultColumnIcon,
   };
 
-  state = {
-    cards: this.props.cards || [],
-  }
-
-  addCard(title){
-    this.setState(state => (
-      {
-        cards: [
-          ...state.cards,
-          {
-            key: state.cards.length ? state.cards[state.cards.length-1].key + 1 : 0,
-            title,
-          },
-        ],
-      }
-    ));
-  }
-
   render() { 
-    const { title, icon } = this.props;
+    const {title, icon} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{title}
@@ -48,7 +30,7 @@ class Column extends React.Component {
             <Icon name={icon} />
           </span>
         </h3>
-        <div className={styles.cards}>
+        {/*<div className={styles.cards}>
           {this.state.cards.map((cardData) => (
             <Card key={cardData.id} {...cardData} />
           ))}
@@ -58,7 +40,7 @@ class Column extends React.Component {
             text={settings.cardCreatorText}
             action={title => this.addCard(title)}
           />
-        </div>
+          </div>*/}
       </section>
     );
   }
