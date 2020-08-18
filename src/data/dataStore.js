bf0a100dc683460ab15b2a6d34f6433da0f97229
Emilia Piechocka <couchcodergirl@gmail.com>
2020-08-18 14:40:12 +0200

    Add search component

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -12,6 +12,7 @@ export const settings = {
   },
   defaultListDescription: '<p>I can do all the things!!!</p>',
   defaultColumnIcon: 'list-alt',
+  defaultCardIcon: 'cat',
 };
 
 export const pageContents = {
