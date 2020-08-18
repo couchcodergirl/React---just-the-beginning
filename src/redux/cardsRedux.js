15a0ef2274cedc9094de1bbdc2f113801e4b7436
Emilia Piechocka <couchcodergirl@gmail.com>
2020-08-18 16:51:41 +0200

    Toying with search

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,7 +1,7 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
+export const getCardsForColumn = ({cards}, columnId, searchString) => cards.filter(card => card.columnId == columnId && new RegExp(searchString, 'i').test(card.title));
 // export const getCardsForSearch = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
 {/*export const getCardsForSearch = ({ cards, columns, lists} , searchString) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).map(card => ({
   ...card,
